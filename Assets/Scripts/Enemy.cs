860580a84bb568f2b66f3bc377290916e9257caf
Paul Kohan <pkohan@whitepages.com>
2015-03-04 22:20:22 -0800

    spawn enemies

---
+++ Assets/Scripts/Enemy.cs
@@ -0,0 +1,18 @@
+﻿using UnityEngine;
+using System.Collections;
+
+public class Enemy : MonoBehaviour {
+
+	public float turnSpeedDegrees;
+	private Rigidbody2D rb;
+
+	// Use this for initialization
+	void Start () {
+		rb = (Rigidbody2D) GetComponent(typeof(Rigidbody2D));
+	}
+	
+	// Update is called once per frame
+	void FixedUpdate () {
+		rb.MoveRotation(rb.rotation - turnSpeedDegrees * Time.fixedDeltaTime);
+	}
+}
---
+++ Assets/Scripts/SpawnEnemy.cs
@@ -0,0 +1,23 @@
+﻿using UnityEngine;
+using System.Collections;
+
+public class SpawnEnemy : MonoBehaviour {
+
+	public GameObject enemy;
+	public GameObject player;
+	public float minDistance;
+	public float maxDistance;
+
+	// Update is called once per frame
+	void Update () {
+		if (Input.GetKeyUp(KeyCode.Z)) {
+			Instantiate(enemy, getRandomPosition(), Quaternion.identity);
+		}
+	}
+
+	private Vector3 getRandomPosition() {
+		float xRandom = Random.Range(-maxDistance, maxDistance);
+		float yRandom = Random.Range(minDistance, maxDistance);
+		return new Vector3(player.transform.position.x + xRandom, player.transform.position.y + yRandom, 0);
+	}
+}
