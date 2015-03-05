9f66bf5ffba253acf68ed0ced1d5a3654f6fee4a
Paul Kohan <pkohan@whitepages.com>
2015-03-04 20:13:33 -0800

    Make camera follow the player around

---
+++ Assets/Scripts/PlayerCamera.cs
@@ -0,0 +1,17 @@
+﻿using UnityEngine;
+using System.Collections;
+
+public class PlayerCamera : MonoBehaviour {
+
+	public Transform player;
+
+	// Use this for initialization
+	void Start () {
+
+	}
+	
+	// Update is called once per frame
+	void FixedUpdate () {
+		transform.position = new Vector3(player.position.x, player.position.y, -2);
+	}
+}
