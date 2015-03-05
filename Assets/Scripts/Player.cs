7722ffe5df27668b04377f4acce5f519f8e2c489
Paul Kohan <pkohan@whitepages.com>
2015-03-04 19:36:30 -0800

    Add Everything

---
+++ Assets/Scripts/Player.cs
@@ -0,0 +1,29 @@
+﻿using UnityEngine;
+using System.Collections;
+
+public class Player : MonoBehaviour {
+
+	public int accelForce = 1;
+	public Rigidbody2D rigidBody;
+
+	// Use this for initialization
+	void Start () {
+		rigidBody = (Rigidbody2D) GetComponent(typeof(Rigidbody2D));
+	}
+	
+	// Update is called once per frame
+	void Update () {
+		if (Input.GetKey(KeyCode.W)) {
+			rigidBody.AddForce(new Vector2(0, accelForce));
+		}
+		if (Input.GetKey(KeyCode.S)) {
+			rigidBody.AddForce(new Vector2(0, -accelForce));
+		}
+		if (Input.GetKey(KeyCode.A)) {
+			rigidBody.AddForce(new Vector2(-accelForce, 0));
+		}
+		if (Input.GetKey(KeyCode.D)) {
+			rigidBody.AddForce(new Vector2(accelForce, 0));
+		}
+	}
+}
