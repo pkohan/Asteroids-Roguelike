4219ed4bbcb209a026df78587b727712ec227bab
Paul Kohan <pkohan@whitepages.com>
2015-03-04 20:41:12 -0800

    Scrolling Background!

---
+++ Assets/Scripts/Background.cs
@@ -0,0 +1,18 @@
+﻿using UnityEngine;
+using System.Collections;
+
+public class Background : MonoBehaviour {
+
+	public Transform player;
+	private Renderer rend;
+
+	void Start() {
+		rend = (Renderer) GetComponent(typeof(Renderer));
+	}
+
+	// Update is called once per frame
+	void Update () {
+		rend.material.mainTextureOffset = new Vector2(player.position.x, player.position.y);
+
+	}
+}
